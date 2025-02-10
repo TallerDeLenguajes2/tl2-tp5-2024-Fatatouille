13e4a421fe3cf8afd3a9c41d0444b2c5c0921f34
Fatatouille <franciscomiguelgimenez708@gmail.com>
2025-02-10 19:42:25 -0300

    a

--- obj/Debug/net8.0/tl2-tp5-2024-Fatatouille.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp5-2024-Fatatouille.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp5-2024-Fatatouille")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f77322b3cb3640c42165b42806d0e2906cb09996")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+624238d2dc8a3e7e6b7b61d652e06e0580ff8bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp5-2024-Fatatouille")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp5-2024-Fatatouille")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
